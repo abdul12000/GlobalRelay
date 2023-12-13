0e20c6fb013c2c0ab5bf93291899f8c10def48e3
lateef abdulsalam <abdulsalam12000@hotmail.com>
2023-12-13 04:20:12 +0000

    complete code

--- src/main/java/newPack/Car.java
+++ src/main/java/newPack/Car.java
@@ -4,43 +4,48 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class Car2 {
+public class Car {
 
     //    Create a Car class that contains company, model and year of manifacturing.
 //    Create a getTotalCarsByCompany() method that takes list as a parameter.
 //    The method should return Map<String,Integer> whereby the key is the company
 //    and the value is the total number of cars per company
-    private  String company;
-    private String  model;
+    private String company;
+    private String model;
     private int yofM;
 
-    public Car2(String company, String model, int yofM) {
+    public Car(String company, String model, int yofM) {
         this.company = company;
         this.model = model;
         this.yofM = yofM;
     }
+
     private String getCompany() {
         return company;
     }
-    public static Map<String, Integer> getTotalCarsByCompany(List<Car2> carList){
-    Map<String, Integer> totalCarsByCompany = new HashMap<>();
-    for(Car2 car : carList){
-        String company = car.getCompany();
-        totalCarsByCompany.put(company, totalCarsByCompany.getOrDefault(company, 0),+1);
+
+    public static Map<String, Integer> getTotalCarsByCompany(List<Car> carList) {
+        Map<String, Integer> totalCarsByCompany = new HashMap<>();
+        for (Car car : carList) {
+            String company = car.getCompany();
+            totalCarsByCompany.put(company, totalCarsByCompany.getOrDefault(company, 0) + 1);
+        }
+        return totalCarsByCompany;
+
+
     }
-    return totalCarsByCompany;
+
+    public static void main(String[] args) {
+        List<Car> cars = List.of(
+                new Car("Mercedes", "benz", 2022),
+                new Car("Honda", "Honda2", 2016),
+                new Car("Honda", "Honda4", 2016),
+                new Car("Toyota", "prius", 2017)
+        );
+
+        Map<String, Integer> totalCarsByCompany = Car.getTotalCarsByCompany(cars);
+        totalCarsByCompany.forEach((company, count) -> System.out.println(company + " total number of cars is : " + count));
+    }
 
 
 }
-
-    public static void main(String[] args) {
-        List<Car2> cars = List.of(
-                new Car2("Mercedes", "benz", 2022),
-        new Car2("Honda", "Honda2", 2016),
-        new Car2("toyota", "prius", 2017)
-        );
-
-    }
-
-
-    }
--- src/main/java/newPack/GRelay.java
+++ src/main/java/newPack/GRelay.java
@@ -1,2 +1,10 @@
-package newPack;public class GRelay {
+package newPack;
+
+public class GRelay {
+
+//    Swapnali Bhansali 10:07
+//    Create a Car class that contains company, model and year of manifacturing.
+//    Create a getTotalCarsByCompany() method that takes list as a parameter.
+//    The method should return Map<String,Integer> whereby the key is the company
+//    and the value is the total number of cars per company
 }
